a19fa7205621e9b883f8b8ee569d7f7ebd9da6cb
Palina <polina.sofina.20@mail.ru>
2024-10-02 19:00:27 +0500

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,50 @@
+class Animal:
+    alive = True        # живой
+    fed = False         # накормленный
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        self.food = isinstance(food, Plant)
+        if food.edible == True:
+            Animal.fed = True
+            print(f'{self.name} съел {food.name}')
+        elif food.edible == False:
+            Animal.alive = False
+            print(f'{self.name} не стал есть {food.name}')
+
+
+class Mammal(Animal):
+    pass
+
+class Predator(Animal):
+    pass
+
+
+class Plant:
+    edible = False        # съедобный
+    def __init__(self, name):
+        self.name = name
+
+class Fruit(Plant):
+    edible = True
+
+class Flower(Plant):
+    pass
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+print()
+print(a1.alive)
+print(a2.fed)
+print()
+a1.eat(p1)
+a2.eat(p2)
+print()
+print(a1.alive)
+print(a2.fed)
